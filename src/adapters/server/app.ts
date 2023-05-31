867d10c66a3fd91cb5b11941423e5ce5de01faeb
Italo Renan <italorenan225@gmail.com>
2023-05-31 08:42:39 -0300

    add route in app

--- src/adapters/server/app.ts
+++ src/adapters/server/app.ts
@@ -1,7 +1,8 @@
 import expess, { json, urlencoded } from 'express';
+import route from './routes';
 
 const app = expess();
 app.use(json());
 app.use(urlencoded({ extended: true }));
-
+app.use('/api', route);
 export default app;
