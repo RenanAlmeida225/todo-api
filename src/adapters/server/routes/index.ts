8b6266d5453b91b645d9a6d509d83641bc33488d
Italo Renan <italorenan225@gmail.com>
2023-06-01 08:51:49 -0300

    added route for find one todo

--- src/adapters/server/routes/index.ts
+++ src/adapters/server/routes/index.ts
@@ -6,5 +6,6 @@ const route = Router();
 const todoController = new TodoController(todoServiceFactory());
 
 route.post('/save', (req, res) => todoController.save(req, res));
+route.get('/find/:id', (req, res) => todoController.find(req, res));
 
 export default route;
