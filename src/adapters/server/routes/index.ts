6789bc39909bfcc3344b22d29ab62c976550b211
Italo Renan <italorenan225@gmail.com>
2023-06-01 09:07:17 -0300

    added route for get all todos

--- src/adapters/server/routes/index.ts
+++ src/adapters/server/routes/index.ts
@@ -7,5 +7,6 @@ const todoController = new TodoController(todoServiceFactory());
 
 route.post('/save', (req, res) => todoController.save(req, res));
 route.get('/find/:id', (req, res) => todoController.find(req, res));
+route.get('/list', (_req, res) => todoController.list(res));
 
 export default route;
