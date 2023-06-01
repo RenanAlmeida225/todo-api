adb713aeba046cd2d5a2353de5e5225eec28b8b7
Italo Renan <italorenan225@gmail.com>
2023-06-01 10:03:48 -0300

    added route for delete todo

--- src/adapters/server/routes/index.ts
+++ src/adapters/server/routes/index.ts
@@ -9,5 +9,6 @@ route.post('/save', (req, res) => todoController.save(req, res));
 route.get('/find/:id', (req, res) => todoController.find(req, res));
 route.get('/list', (_req, res) => todoController.list(res));
 route.put('/complete/:id', (req, res) => todoController.complete(req, res));
+route.delete('/delete/:id', (req, res) => todoController.delete(req, res));
 
 export default route;
