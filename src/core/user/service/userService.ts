42d73f3767ba7faf3bd388dda298a192ba8cc8e6
Italo Renan <italorenan225@gmail.com>
2023-06-05 15:42:45 -0300

    created a function to register new user

--- src/core/user/service/userService.ts
+++ src/core/user/service/userService.ts
@@ -1,13 +1,39 @@
+import { IDateFormat } from '../../util/model/iDateFormat';
+import { ITokenGenerator } from '../../util/model/iTokenGenerator';
+import { IUserRepository } from '../model/iUserRepository';
 import { IUserService } from '../model/iUserService';
 import { User } from '../model/user';
 
 export class UserService implements IUserService {
-	register({
+	#repository: IUserRepository;
+	#dateFormat: IDateFormat;
+	#tokenGenerator: ITokenGenerator;
+	constructor(
+		repository: IUserRepository,
+		dateFormat: IDateFormat,
+		tokenGenerator: ITokenGenerator,
+	) {
+		this.#repository = repository;
+		this.#dateFormat = dateFormat;
+		this.#tokenGenerator = tokenGenerator;
+	}
+	get repository() {
+		return this.#repository;
+	}
+	async register({
 		name,
 		email,
 		password,
-	}: Omit<User, 'id' | 'createAt'>): Promise<string> {
-		throw new Error('Method not implemented.');
+	}: Omit<User, 'id' | 'createAt'>): Promise<string | null> {
+		const emailExists = await this.repository.find(email);
+		if (emailExists) return null;
+		await this.#repository.save({
+			name,
+			email,
+			password,
+			createAt: this.#dateFormat.format(new Date()),
+		});
+		return this.#tokenGenerator.encrypt(email);
 	}
 	login({
 		email,
