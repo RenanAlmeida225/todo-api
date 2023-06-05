67eb551ba0502cc2eb8f0facadea732bc69d4ee1
Italo Renan <italorenan225@gmail.com>
2023-06-05 15:39:06 -0300

    changed params in function find in repository interface

--- src/core/user/model/iUserRepository.ts
+++ src/core/user/model/iUserRepository.ts
@@ -2,8 +2,5 @@ import { User } from './user';
 
 export interface IUserRepository {
 	save({ name, email, password, createAt }: Omit<User, 'id'>): Promise<void>;
-	find({
-		email,
-		password,
-	}: Pick<User, 'email' | 'password'>): Promise<User | null>;
+	find(email: string): Promise<string | null>;
 }
